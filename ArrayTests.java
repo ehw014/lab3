a0a97b29b623c7aeb50139ff24e8d8d17e9957f2
ehw014 <35472009+ehw014@users.noreply.github.com>
2022-10-12 14:43:57 -0700

    Update Array Methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,22 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 11,12,13,14 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 14,13,12,11 }, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 11, 12, 13, 14 };
+    assertArrayEquals(new int[]{ 14, 13, 12, 11 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAvg(){
+    double delta = 0.05;
+    double[] input1 = {1,0,2,3};
+    assertEquals(2, ArrayExamples.averageWithoutLowest(input1), 0.05);
   }
 }
